b6e6e96fb98fd4bf18c7300d273868f465bda724
dylanclement <dylanclement@gmail.com>
2012-04-28 20:12:55 +1000

    add signed in user methods to sessionhelper.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,4 +3,16 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
     current_user = user
   end
+  
+  def current_user?(user)
+    user == current_user
+  end
+  
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_path, notice: "Please sign in."
+    end
+  end
+  
 end
